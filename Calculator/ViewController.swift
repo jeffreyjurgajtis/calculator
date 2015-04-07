b57b5b99a5b3ac63579a9a711f1cce85ba23f4dd
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2015-04-06 19:39:30 -0500

    Complete calculator functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,17 +12,60 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
     
-    var userIsTypingNumber: Bool = false
+    var userIsTypingNumber = false
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        
+
 
         if userIsTypingNumber {
             display.text = display.text! + digit
         } else {
             display.text = digit
             userIsTypingNumber = true
+        }
+    }
+
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        switch operation {
+        case "×": performOperation({ $0 * $1 })
+        case "÷": performOperation({ $1 / $0 })
+        case "+": performOperation({ $0 + $1 })
+        case "−": performOperation({ $1 - $0 })
+        case "√": performOperation({ sqrt($0) })
+        default: break
+        }
+    }
+
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    var operandStack = Array<Double>()
+
+    @IBAction func enter() {
+        userIsTypingNumber = false
+        operandStack.append(displayValue)
+    }
+
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsTypingNumber = false
         }
     }
 }
